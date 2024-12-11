fb6d972f24cfed9ed0bd6f647cad08cce2c08152
lucarl07 <junitinho2007@gmail.com>
2024-12-11 15:13:21 -0300

    🔧 build: Set 5 absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      fonts: "/src/assets/fonts",
+      images: "/src/assets/images",
+      components: "/src/components", 
+      utils: "/src/utils", 
+    }
+  },
 })
