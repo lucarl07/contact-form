e79a80cdc440e5e02fcd92df3f55bdf98ce78967
lucarl07 <junitinho2007@gmail.com>
2024-12-11 22:21:00 -0300

    🚧 Basic implementation of Controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import Container from 'components/Container/Container';
-import Title from './components/Title/Title';
-import Form from './components/Form/Form';
-import Button from './components/Button/Button';
+import Title from 'components/Title/Title';
+import Form from 'components/Form/Form';
+import Control from 'components/Control'
+import Button from 'components/Button/Button';
 
 export default function App() {
   return (
     <Container>
       <Title>Contact Us</Title>
       <Form>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum inventore, voluptatem quidem possimus dignissimos facere totam! Aliquid suscipit ducimus minus quo qui praesentium dolores totam eum expedita, libero ex. Consectetur.</p>
+        <Control.Root error="" field="firstName">
+          <Control.Label>First Name *</Control.Label>
+          <Control.Input type="text" />
+          <Control.Error />
+        </Control.Root>
+        <Control.Root error="" field="queryType">
+          <Control.Label>Query Type *</Control.Label>
+          <div className="flex space-x-3">
+            <Control.Input type="radio" id="enquiry" value="General Enquiry" />
+            <Control.Input type="radio" id="support" value="Support Request" />
+          </div>
+        </Control.Root>
         <Button type="submit">Submit</Button>
       </Form>
     </Container>
