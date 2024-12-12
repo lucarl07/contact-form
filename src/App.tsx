0b98ec91e24c7daaa9b8c1f5599b0edb17c17c89
lucarl07 <junitinho2007@gmail.com>
2024-12-11 22:48:18 -0300

    ✨ feat: Put different Controls at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,22 @@ import Button from 'components/Button/Button';
 export default function App() {
   return (
     <Container>
-      <Title>Contact Us</Title>
       <Form>
-        <Control.Root error="" field="firstName">
-          <Control.Label>First Name *</Control.Label>
+        <Title>Contact Us</Title>
+        <div className="flex space-x-3">
+          <Control.Root error="" field="firstName">
+            <Control.Label>First Name *</Control.Label>
+            <Control.Input type="text" />
+            <Control.Error />
+          </Control.Root>
+          <Control.Root error="" field="lastName">
+            <Control.Label>Last Name *</Control.Label>
+            <Control.Input type="text" />
+            <Control.Error />
+          </Control.Root>
+        </div>
+        <Control.Root error="" field="email">
+          <Control.Label>Email Address *</Control.Label>
           <Control.Input type="text" />
           <Control.Error />
         </Control.Root>
@@ -20,6 +32,11 @@ export default function App() {
             <Control.Input type="radio" id="enquiry" value="General Enquiry" />
             <Control.Input type="radio" id="support" value="Support Request" />
           </div>
+        </Control.Root>
+        <Control.Root error="" field="message">
+          <Control.Label>Message *</Control.Label>
+          <Control.TextArea rows={3} />
+          <Control.Error />
         </Control.Root>
         <Button type="submit">Submit</Button>
       </Form>
