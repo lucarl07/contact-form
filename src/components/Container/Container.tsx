8d8dd8d90b588cd38f93e2f44f19d18805dbf605
lucarl07 <junitinho2007@gmail.com>
2024-12-11 22:44:05 -0300

    🗃 raw: Set the Container's width to 672px

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 function Container({ children }: ContainerProps) {
   return (
     <section 
-      className="absolute top-1/2 left-1/2 flex flex-col p-8 rounded-2xl w-[896px] bg-white"
+      className="absolute top-1/2 left-1/2 flex flex-col p-8 rounded-2xl w-[672px] bg-white"
       style={{ transform: 'translate(-50%, -50%)' }}
     >
       {children}
