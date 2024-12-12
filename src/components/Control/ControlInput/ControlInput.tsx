1e1cc124b277628f91d1ee500af2e5dfd37a28ab
lucarl07 <junitinho2007@gmail.com>
2024-12-11 23:58:13 -0300

    ♻ refactor: Control.Input's conditional rendering

--- src/components/Control/ControlInput/ControlInput.tsx
+++ src/components/Control/ControlInput/ControlInput.tsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import ControlContext from 'contexts/ControlContext'
 
-type ControlInputProps = React.InputHTMLAttributes<HTMLInputElement>
+interface ControlInputProps 
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  field?: string
+  error?: string
+}
 
-function ControlInput(props: ControlInputProps) {
-  const { field, error } = React.useContext(ControlContext)
-  const borderColor = error.length > 0 ? 'border-lc-red' : 'border-gray-400'
+function Radio({ field, id, value }: ControlInputProps) {
+  return (
+    <div className="flex items-center px-5 w-full h-12 gap-3 border border-gray-400 rounded-md">
+      <input 
+        type='radio' id={id} name={field} 
+        className="border border-gray-300 rounded-full size-4 accent-lc-green-600" />
+      <label htmlFor={id}>
+        {value}
+      </label>
+    </div>
+  )
+}
 
-  if (props.type === 'radio') {
-    return (
-      <div className="flex items-center px-5 w-full h-12 gap-3 border border-gray-400 rounded-md">
-        <input 
-          {...props} 
-          id={props.id} 
-          name={field} 
-          className="border border-gray-300 rounded-full size-4 accent-lc-green-600" />
-        <label htmlFor={props.id}>
-          {props.value}
-        </label>
-      </div>
-    )
-  }
+function DefaultInput({ field, error, type }: ControlInputProps) {
+  const borderColor = error!.length > 0 ? 'border-lc-red' : 'border-gray-400'
   
   return (
     <input 
-      {...props} id={field} name={field}
+      type={type} id={field} name={field}
       className={`px-4 w-full border ${borderColor} rounded-md h-12`} />
   )
 }
 
-export default ControlInput
+export default function ControlInput(props: ControlInputProps) {
+  const { field, error } = React.useContext(ControlContext)
+
+  switch (props.type) {
+    case 'radio':
+      return <Radio field={field} id={props.id} value={props.value} />
+    default:
+      return <DefaultInput field={field} error={error} type={props.type || 'text'} />
+  }
+}
