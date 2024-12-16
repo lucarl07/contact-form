319e00d1276dd2119000345a28c62c65a9aa1e23
lucarl07 <junitinho2007@gmail.com>
2024-12-16 17:16:46 -0300

    🗃 raw: Add rest props to component Control.Input

--- src/components/Control/ControlInput/ControlInput.tsx
+++ src/components/Control/ControlInput/ControlInput.tsx
@@ -3,37 +3,38 @@ import ControlContext from 'contexts/ControlContext'
 
 interface ControlInputProps 
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  field?: string
+  field?: string /* Name of the input field or group */
+  desc?: string /* Description for individual radios on a group */
   error?: string
 }
 
-function Radio({ field, id, value }: ControlInputProps) {
+function Radio({ field, desc, ...rest }: ControlInputProps) {
   return (
     <div className="flex items-center px-5 w-full h-12 gap-3 border border-gray-400 rounded-md">
       <input 
-        type='radio' id={id} name={field} 
+        type='radio' name={field} id={rest.id} value={rest.value} {...rest}
         className="border border-gray-300 rounded-full size-4 accent-lc-green-600" />
-      <label htmlFor={id}>
-        {value}
+      <label htmlFor={rest.id}>
+        {desc}
       </label>
     </div>
   )
 }
 
-function Checkbox({ field }: ControlInputProps) {
+function Checkbox({ field, ...rest }: ControlInputProps) {
   return (
     <input 
-      type='checkbox' id={field} name={field} 
+      type='checkbox' id={field} name={field} {...rest}
       className="size-4 accent-lc-green-600" />
   )
 }
 
-function DefaultInput({ field, error, type }: ControlInputProps) {
+function DefaultInput({ field, error, type, ...rest }: ControlInputProps) {
   const borderColor = error!.length > 0 ? 'border-lc-red' : 'border-gray-400'
   
   return (
     <input 
-      type={type} id={field} name={field}
+      type={type} id={field} name={field} {...rest}
       className={`px-4 w-full border ${borderColor} rounded-md h-12`} />
   )
 }
@@ -43,10 +44,10 @@ export default function ControlInput(props: ControlInputProps) {
 
   switch (props.type) {
     case 'radio':
-      return <Radio field={field} id={props.id} value={props.value} />
+      return <Radio field={field} id={props.id} value={props.value} {...props} />
     case 'checkbox':
-      return <Checkbox field={field} />
+      return <Checkbox field={field} {...props} />
     default:
-      return <DefaultInput field={field} error={error} type={props.type || 'text'} />
+      return <DefaultInput field={field} error={error} type={props.type || 'text'} {...props} />
   }
 }
