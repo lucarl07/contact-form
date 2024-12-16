e173aac6dd39d4141667bedd4ff38d8dd417daec
lucarl07 <junitinho2007@gmail.com>
2024-12-16 17:18:19 -0300

    ✨ feat: Handle form state at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+// Hooks:
+import useFormState from 'hooks/useFormState'
+
+// Components:
 import Container from 'components/Container/Container';
 import Title from 'components/Title/Title';
 import Form from 'components/Form/Form';
@@ -5,6 +9,14 @@ import Control from 'components/Control'
 import Button from 'components/Button/Button';
 
 export default function App() {
+  const [formValues, setFormValues] = useFormState()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    console.log(e.target)
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: { value, error: '' } })
+  }
+
   return (
     <Container>
       <Form>
@@ -12,30 +24,49 @@ export default function App() {
         <div className="flex space-x-3">
           <Control.Root error="" field="firstName">
             <Control.Label>First Name *</Control.Label>
-            <Control.Input type="text" />
+            <Control.Input 
+              type="text"
+              value={formValues.firstName.value}
+              onChange={handleChange} />
             <Control.Error />
           </Control.Root>
           <Control.Root error="" field="lastName">
             <Control.Label>Last Name *</Control.Label>
-            <Control.Input type="text" />
+            <Control.Input 
+              type="text"
+              value={formValues.lastName.value}
+              onChange={handleChange} />
             <Control.Error />
           </Control.Root>
         </div>
         <Control.Root error="" field="email">
           <Control.Label>Email Address *</Control.Label>
-          <Control.Input type="text" />
+          <Control.Input 
+            type="text"
+            value={formValues.email.value}
+            onChange={handleChange} />
           <Control.Error />
         </Control.Root>
         <Control.Root error="" field="queryType">
           <Control.Label>Query Type *</Control.Label>
           <div className="flex space-x-3">
-            <Control.Input type="radio" id="enquiry" value="General Enquiry" />
-            <Control.Input type="radio" id="support" value="Support Request" />
+            <Control.Input 
+              type="radio" id="enquiry" value="enquiry"
+              desc="General Enquiry"
+              onChange={handleChange} />
+            <Control.Input 
+              type="radio" id="support" value="support"
+              desc="Support Request"
+              onChange={handleChange} />
           </div>
+          <Control.Error />
         </Control.Root>
         <Control.Root error="" field="message">
           <Control.Label>Message *</Control.Label>
-          <Control.TextArea rows={3} />
+          <Control.TextArea 
+            rows={3} 
+            value={formValues.message.value}
+            onChange={handleChange} />
           <Control.Error />
         </Control.Root>
         <Control.Root error="" field="agreeOnContact">
