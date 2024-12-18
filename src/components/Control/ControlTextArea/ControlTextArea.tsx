b070dc1ccd692b48c1b23732ecb84f1613d22790
lucarl07 <junitinho2007@gmail.com>
2024-12-18 10:48:54 -0300

    💄 feat: Stylize ControlTextArea at different states & remove resizing

--- src/components/Control/ControlTextArea/ControlTextArea.tsx
+++ src/components/Control/ControlTextArea/ControlTextArea.tsx
@@ -5,12 +5,16 @@ type ControlTextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
 function ControlTextArea(props: ControlTextAreaProps) {
   const { field, error } = React.useContext(ControlContext)
-  const borderColor = error.length > 0 ? 'border-lc-red' : 'border-gray-400'
+  const borderColor = error!.length > 0 ? 'border-lc-red' : 'border-gray-400'
 
   return (
     <textarea
       {...props} id={field} name={field}
-      className={`p-4 w-full border ${borderColor} rounded-md`} />
+      className={`
+        p-4 w-full border ${borderColor} rounded-md resize-none
+        hover:border-lc-green-600 
+        focus:border-lc-green-600 focus:outline-none
+      `} />
   )
 }
 
