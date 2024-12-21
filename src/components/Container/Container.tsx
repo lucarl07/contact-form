e01d6b1f9529fd23d5b98f3222fa48003ab15620
lucarl07 <junitinho2007@gmail.com>
2024-12-21 14:23:54 -0300

    📱 Add responsivity for screens below the 'md' breakpoint
    md = Medium, width of 768px or higher

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -6,13 +6,11 @@ interface ContainerProps {
 
 function Container({ children }: ContainerProps) {
   return (
-    <section 
-      className="
-        absolute top-1/2 left-1/2 flex flex-col mt-20 p-8 gap-6 w-11/12 rounded-2xl bg-white
-        md:my-0 md:w-[672px]
-      "
-      style={{ transform: 'translate(-50%, -50%)' }}
-    >
+    <section className="
+      flex flex-col mx-4 my-4 p-8 gap-6 rounded-2xl bg-white
+      md:absolute md:-translate-x-1/2 md:-translate-y-1/2 md:top-1/2 md:left-1/2 
+      md:m-0 md:w-[672px]
+    ">
       {children}
     </section>
   )
