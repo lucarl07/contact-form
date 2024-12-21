5d8fbe57eb85896b2596a2a12fab80fab64d6a79
lucarl07 <junitinho2007@gmail.com>
2024-12-21 14:25:29 -0300

    📱 Add responsivity for screens below the 'sm' breakpoint
    sm = Small, width of 640px or higher

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -8,8 +8,9 @@ function Container({ children }: ContainerProps) {
   return (
     <section className="
       flex flex-col mx-4 my-4 p-8 gap-6 rounded-2xl bg-white
-      md:absolute md:-translate-x-1/2 md:-translate-y-1/2 md:top-1/2 md:left-1/2 
-      md:m-0 md:w-[672px]
+      sm:absolute sm:-translate-x-1/2 sm:-translate-y-1/2 sm:top-1/2 sm:left-1/2 
+      sm:m-0 sm:w-11/12 
+      md:w-[672px]
     ">
       {children}
     </section>
