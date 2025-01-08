7a82dd0e3363e45e8ec51b5c4cbe1c1c7b703d55
lucarl07 <junitinho2007@gmail.com>
2025-01-08 16:35:34 -0300

    📱 Set background color for active Buttons in <md devices

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,8 +9,8 @@ export default function Button({ children, ...rest }: ButtonProps) {
   return (
     <button {...rest} className="
       p-4 w-full rounded-md bg-lc-green-600 font-medium text-white 
-      transition-colors
-      hover:bg-lc-grey-900
+      transition-colors active:bg-lc-grey-900
+      md:hover:bg-lc-grey-900
     ">
       {children}
     </button>
